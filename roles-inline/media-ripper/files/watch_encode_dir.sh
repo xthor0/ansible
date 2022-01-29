b3cc78a8d58f6917e3d1d5980f4d1575c53b6c44
Ben Brown <xthor@xthorsworld.com>
2022-01-29 14:17:04 -0700

    working on breaking the ripper scripts up into smaller tasks

--- roles-inline/media-ripper/files/watch_encode_dir.sh
+++ roles-inline/media-ripper/files/watch_encode_dir.sh
@@ -2,6 +2,7 @@
 
 watch_dir=/scratch/watch
 
-inotifywait -e close_write -m /scratch/watch --format '%f' | while read file; do
-    encode_file.sh "${file}"
+inotifywait -e close_write -m /scratch/watch --format '%f' | while read watch_dir file; do
+    # encode_file.sh "${file}"
+    echo "encode_file.sh \"${watch_dir}/${file}\""
 done
