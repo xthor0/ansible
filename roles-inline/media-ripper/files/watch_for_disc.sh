e51574c28f8e8fc6a12fc92b64abdda4e55734f9
Ben Brown <xthor@xthorsworld.com>
2022-01-29 14:49:26 -0700

    working on breaking the ripper scripts up into smaller tasks

---
+++ roles-inline/media-ripper/files/watch_for_disc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function discpresent() {
+  echo
+  tput sc
+  while true; do
+    file -s /dev/sr0 >& /dev/null
+    if [ $? -eq 0 ]; then
+      tput el
+      echo "Disc loaded!"
+      return
+    else
+      echo -n "Waiting for disc..."
+      tput rc
+      sleep 5
+    fi
+  done
+}
+
+while true; do
+    discpresent
+    ripdisc.sh
+done
+
+# it loops. forever.
