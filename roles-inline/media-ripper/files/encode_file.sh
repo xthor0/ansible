fe8f735f33dc0ea2fcd94e853d740d1e93f46c0b
Ben Brown <xthor@xthorsworld.com>
2022-01-29 20:44:25 -0700

    bugfix

--- roles-inline/media-ripper/files/encode_file.sh
+++ roles-inline/media-ripper/files/encode_file.sh
@@ -66,7 +66,7 @@ width=$(mediainfo --Inform="Video;%Width%" "${input_file}")
 case ${width} in
     720) preset_import_file="${HOME}/.handbrake-presets/1080p_qsv.json"; preset="1080p_qsv"; extension="mp4"; suboutput="DVD" ;;
     1920) preset_import_file="${HOME}/.handbrake-presets/1080p_qsv.json"; preset="1080p_qsv"; extension="mp4"; suboutput="1080p" ;;
-    3840) preset_import_file="${HOME}/.handbrake-presets/1080p_qsv.json"; preset="1080p_qsv"; extension="mp4"; suboutput="4k" ;;
+    3840) preset_import_file="${HOME}/.handbrake-presets/4k_qsv.json"; preset="4k_qsv"; extension="mp4"; suboutput="4k" ;;
     *) preset_import_file="${HOME}/.handbrake-presets/1080p_qsv.json"; preset="1080p_qsv"; extension="mp4"; suboutput="unknown";;
 esac
 
