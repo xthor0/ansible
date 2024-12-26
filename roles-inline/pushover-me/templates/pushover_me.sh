a838221a2dd0e398896f512934aa0c50a66899c4
Ben Brown <xthor@xthorsworld.com>
2024-12-26 15:26:03 -0700

    fixes to pushover role

--- roles-inline/pushover-me/templates/pushover_me.sh
+++ roles-inline/pushover-me/templates/pushover_me.sh
@@ -6,4 +6,11 @@ curl -s \
   --form-string "user={{ pushover_usertoken }}" \
   --form-string "title=$(whoami)@${HOSTNAME}" \
   --form-string "message=Notification: $*" \
-https://api.pushover.net/1/messages.json
+https://api.pushover.net/1/messages.json > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "Message sent successfully."
+else
+  echo "Curl exited with a non-zero status."
+fi
+
